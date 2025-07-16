780f9c6b9bcc4364af4d827f492b12e6a821178c
Somnath Kundu <kundusomnath610@gmail.com>
2025-07-16 20:42:00 +0530

    initial commit

--- myapp/myapp/urls.py
+++ myapp/myapp/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.views import test1
+from django.views import test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('views/', test1)
+    path('views/', test)
 ]
