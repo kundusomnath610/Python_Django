380b752add89b1f5e59e2aa4ab49b2e93e0dc63d
Somnath Kundu <kundusomnath610@gmail.com>
2025-07-20 17:43:46 +0530

    commit

--- myapp/myapp/views.py
+++ myapp/myapp/views.py
@@ -1,15 +1,18 @@
 from django.http import HttpResponse
 import datetime
 
+from django.shortcuts import render
+
 
 def test(request):
     print("Test function..")
     date = datetime.datetime.now()
-    return HttpResponse("<h1> Hello this is from python </h1>" + str(date))
+    return render(request, "home.html", {})
 
 
 def about(request):
-    return HttpResponse("<h1> This is about page</h1>")
+    # return HttpResponse("<h1> This is about page</h1>")
+    return render(request, "index.html", {})
 
 def services(request):
     return HttpResponse("<h1> This is services page </h1>")
